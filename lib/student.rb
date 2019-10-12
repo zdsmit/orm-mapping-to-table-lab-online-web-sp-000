ea18742a4f67dde8cf55207fa61706abe98f8137
Zach  Smith <zdsmith001@gmail.com>
2019-10-12 05:13:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,8 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE students (
-      id INTEGER PRIMARY KEY
-      name TEXT
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       grade INTEGER
     )
     SQL
