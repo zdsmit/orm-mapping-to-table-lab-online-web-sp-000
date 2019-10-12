7298a15d6f4ab85b258c924dbdeee201bec692a2
Zach  Smith <zdsmith001@gmail.com>
2019-10-12 05:01:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,8 +3,8 @@ class Student
   attr_reader :id
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
 
   def initialize(name, grade, id=nil)
-  
+
 end
