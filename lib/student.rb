22ae989dfd76ac43f6241d9925f6fa61d308a3f7
Zach  Smith <zdsmith001@gmail.com>
2019-10-12 05:06:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,12 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL CREATE TABLE students (
+      id INTEGER PRIMARY KEY
+      name TEXT
+      grade INTEGER
+    );
+    SQL
   end
 
   def self.drop_table
